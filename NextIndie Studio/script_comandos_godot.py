2cfd653ce3edbf6e20a3ee3ba106ab80b4b84b14
Caio Queiroz <caioq8421@gmail.com>
2026-08-28 16:45:12 -0300

    Atualizando

--- NextIndie Studio/script_comandos_godot.py
+++ NextIndie Studio/script_comandos_godot.py
@@ -1,22 +1,25 @@
 import pandas as pd 
 
 df = pd.read_csv(
-    'comandos_godot.csv',
+    'NextIndie Studio/comandos_godot.csv',
     sep = ';'
 )
 
-print(df.head(3))
-
-print(df.iloc[
-    1,
-    1
-])
+# Criando um exemplo de busca pela função de um comando específico
+# print(df.iloc[
+#     1,
+#     1
+# ])
 
 # Usando a função str.contains('var') para buscar pela palavra var contida na coluna sintaxe 
+# print(df[
+#     df['sintaxe'].str.contains('var')
+# ])
+
+# sintaxe
+# função
+# exemplo de uso
 print(df[
-    df['sintaxe'].str.contains('var')
+    df['sintaxe'].str.contains('bounce')
 ])
 
-print(df[
-    df['exemplo de uso'].str.contains('timer', case = False)
-])
