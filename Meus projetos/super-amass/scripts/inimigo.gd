3302eec94b37f0eac3f290360b5f69ad55cae2f8
Caio Queiroz <caioq8421@gmail.com>
2026-09-03 18:00:21 -0300

    Atualizando

--- Meus projetos/super-amass/scripts/inimigo.gd
+++ Meus projetos/super-amass/scripts/inimigo.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+# Definindo a velocidade do inimigo
+var velocidade = 50
+
+func _physics_process(delta):
+	# Resetando a velocidade a cada frame para que não se acumule 
+	velocity.x = 0
+	velocity.y = 0
+	
+	velocity.x += velocidade
+	
+	move_and_collide(velocity * delta)
